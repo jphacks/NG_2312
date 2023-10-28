a8bdadc2a146100b61792bb63691b04000938de7
onoda4480 <123041906+onoda4480@users.noreply.github.com>
2023-10-28 19:43:12 +0900

    Add files via upload

---
+++ python-function/retrun_bot/supabot.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from supabase import Client, create_client
+import os 
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+def base(url,key):
+    from datetime import datetime
+    from supabase import Client, create_client
+    line_id_list = []
+
+    #supabase URL&key
+    SUPABASE_URL = url
+    SUPABASE_KEY = key
+    # Supabaseクライアントを作成
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+    today_time = datetime.now()
+    return_day = today_time.strftime("%Y-%m-%d")
+
+    #data = supabase.table("test_jphack").eq('name',"akiyosi").execute()
+    replay_Rental = supabase.table("Rental").select("lender_id").eq("return_date", return_day).execute()
+    data = replay_Rental.data
+    for item in data:
+        line_id_list.append(item)
+
+    return supabase, return_day, data
+
+supabase, return_day, data= base(url,key)
