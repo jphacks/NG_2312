d496be29fd3419e01fe87d11510b701c860d51a2
suzu-ki <risubonn.11@gmail.com>
2023-10-28 16:04:45 +0900

    望む形でデータを取得できるようにした

--- python-function/ba-code/bookAPI.py
+++ python-function/ba-code/bookAPI.py
@@ -3,26 +3,51 @@ import requests
 import json
 
 base_url = "https://www.googleapis.com/books/v1/volumes"
-# title = "ディープラーニング"
-number = 9784844365174
-params = {
-    "q": f"isbn:{number}", # intitleで本のタイトルから検索
-    "printType": "books",
-    "maxResults": 10
-}
-response = requests.get(base_url, params=params)
 
-if response.status_code == 200:
-    results = json.loads(response.text)
-else:
-    results = None
+BookInfo = {"title":"", "description":"", "author":"", "image_url":""}
 
-# 最初の5冊の情報を表示
-for i, item in enumerate(results["items"][:5]):
-    print(f"Title: {item['volumeInfo']['title']}")
-    print(f"Authors: {item['volumeInfo'].get('authors', ['N/A'])}")
-    print()
 
+def search_Books(number):
+    number = 9784844365174
+    params = {
+        "q": f"isbn:{number}", # intitleで本のタイトルから検索
+        "printType": "books",
+        "maxResults": 10
+    }
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        results = json.loads(response.text)
+    else:
+        results = None
+
+    # 最初の5冊の情報を表示
+    for i, item in enumerate(results["items"][:5]):
+        BookInfo["title"] = item['volumeInfo']['title']
+        BookInfo["description"] = item['volumeInfo'].get('description', [''])
+        authors = item['volumeInfo'].get('authors', [''])
+        
+        if authors == "":
+            BookInfo["author"] = item['volumeInfo'].get('publisher', [''])
+        else:
+            author_result = authors[0]
+            if len(authors) > 2:
+                author_result = '/'.join(authors)
+            BookInfo["author"] = author_result
+        
+        img_url = item['volumeInfo']['imageLinks'].get('smallThumbnail', [''])
+        if img_url == '':
+            BookInfo.pop("image_url")
+        else:
+            BookInfo["image_url"] = img_url
+        
+        print(f"Title: {item['volumeInfo']['title']}")
+        print(f"Authors: {item['volumeInfo'].get('authors', ['N/A'])}")
+    
+    print(BookInfo)
+
+
+search_Books(9784844365174)
 
 # タイトル、説明、画像URL、著者
 # 説明は空でも返す
