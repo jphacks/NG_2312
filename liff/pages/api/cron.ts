c529677035dcce06bc3e9435252772018323e630
Hasegawa-Akito <h.akito1999@gmail.com>
2023-11-06 16:36:48 +0900

    add: メッセージ送信機能実装

--- liff/pages/api/cron.ts
+++ liff/pages/api/cron.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/api/db";
+import { LineStickerMessage, LineTextMessage } from "@/lib/api/line/type";
+import { pushMessage } from "@/lib/api/line/pushMessage";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,7 +10,7 @@ export default async function handler(
   try {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
-    const rental = await prisma.rental.findMany({
+    const rentals = await prisma.rental.findMany({
       where: {
         return_date: {
           gte: today, // 今日以降の日付
@@ -24,8 +26,47 @@ export default async function handler(
         books: true,
       },
     });
+
+    rentals.forEach(async (rental) => {
+      try {
+        const messages = [];
+        const lineId = rental.borrower!.line_id;
+        const lenderName = rental.lender.name;
+        const rentalId = rental.id;
+
+        if (!rental.is_return) {
+          // 催促メッセージ追加
+          const reminderMessage: LineTextMessage = {
+            type: "text",
+            text: `${lenderName}さんから借りている本の返却日は今日です。忘れないように注意してください❗️\n\n詳細はこちら\n${process.env.LIFF_URL}/detail/${rentalId}`,
+          };
+          messages.push(reminderMessage);
+        }
+
+        // アンケート願いメッセージ追加
+        const questionaryMessage: LineTextMessage = {
+          type: "text",
+          text: `${lenderName}さんから借りた本の感想についてアンケートをお願いします❗️❗️❗️\n\nアンケートに答えることで、${lenderName}さんにポイントをプレゼントすることができます。🎁\nアンケートの内容は${lenderName}さんからは見れないので安心してください!!\n\nアンケートはこちら\n${process.env.LIFF_URL}/questionary/${rentalId}`,
+        };
+        messages.push(questionaryMessage);
+
+        // スタンプメッセージ追加
+        const stickerMessage: LineStickerMessage = {
+          type: "sticker",
+          packageId: "6136",
+          stickerId: "10551380",
+        };
+        messages.push(stickerMessage);
+
+        await pushMessage(messages, lineId);
+        console.log("メッセージ送信");
+      } catch (error) {
+        console.log("メッセージ送信に失敗");
+      }
+    });
   } catch (error) {
-    console.log("定期実行中にエラーが発生");
+    console.log("データ取得に失敗");
+  } finally {
+    res.status(200).end();
   }
-  res.status(200).end();
 }
