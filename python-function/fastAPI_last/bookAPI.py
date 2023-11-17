6846be0041836fdea77f0015cc2f0432d6119e98
suzu-ki <risubonn.11@gmail.com>
2023-11-17 20:29:02 +0900

    最新に更新

--- python-function/fastAPI_last/bookAPI.py
+++ python-function/fastAPI_last/bookAPI.py
@@ -8,30 +8,28 @@ def search_Books(number):
     params = {
         "q": f"isbn:{number}",
         "printType": "books",
-        "maxResults": 10
+        "maxResults": 2
     }
     response = requests.get(base_url, params=params)
 
     if response.status_code == 200:
         results = json.loads(response.text)
     else:
-        results = None
+        return BookInfo
 
-    for i, item in enumerate(results["items"][:2]):
-        BookInfo["title"] = item['volumeInfo']['title']
-        BookInfo["description"] = item['volumeInfo'].get('description', [''])
-        authors = item['volumeInfo'].get('authors', [''])
-        
-        if authors == "":
-            BookInfo["author"] = item['volumeInfo'].get('publisher', [''])
-        else:
-            author_result = authors[0]
-            if len(authors) > 2:
-                author_result = '/'.join(authors)
-            BookInfo["author"] = author_result
-        
-        BookInfo["image_url"] = item['volumeInfo']['imageLinks'].get('smallThumbnail', [''])
-        if BookInfo["image_url"] == '':
-            BookInfo.pop("image_url")
+    info = results["items"][0]['volumeInfo']
+    
+    BookInfo["title"] = info['title']
+    BookInfo["description"] = info.get('description', '')
+    authors = info.get('authors', '')
+    
+    if not authors:
+        BookInfo["author"] = info.get('publisher', '')
+    else:
+        BookInfo["author"] = '/'.join(authors)
+    
+    BookInfo["image_url"] = info['imageLinks'].get('smallThumbnail', '')
+    if not BookInfo["image_url"]:
+        BookInfo.pop("image_url", None)
     
     return BookInfo
--- python-function/fastAPI_last/readBacorde.py
+++ python-function/fastAPI_last/readBacorde.py
@@ -2,56 +2,70 @@ import cv2
 import numpy as np
 import bookAPI
 import copy
+import time
+import asyncio
 
 bd = cv2.barcode.BarcodeDetector('./path/sr.prototxt', './path/sr.caffemodel')
 code = []
+BookInfo_list = []
 
 def detect(img1):
     img1 = cv2.resize(img1, (1000, 1000))
-    retval, decoded_info, decoded_type, points = bd.detectAndDecode(img1)
-    for i in decoded_info:
-        if len(i) == 13 and (not i in code) and i.startswith('978'):
-            print("i:", i)
-            code.append(i)
+    retval, decoded_info, _, _ = bd.detectAndDecode(img1)
+    if retval:
+        for i in decoded_info:
+            if len(i) == 13 and (not i in code) and i.startswith('978'):
+                print("i:", i)
+                code.append(i)
+                item = bookAPI.search_Books(i)
+                BookInfo_list.append(copy.deepcopy(item))
 
-def search_Image(img):
-    height, width, channels = img.shape[:3]
-    
-    flag_h = True
-    for k in [300, 500]:
-        a = 0
+async def process_image(img, a, b, c, d):
+    img1 = img[a : b, c: d]
+    await asyncio.to_thread(detect, img1)
+
+async def search_Image(img):
+    await asyncio.gather(
+        search(img, 300),
+        search(img, 500)
+    )
+
+async def search(img, k):
+    hw = 1000
+    a = 0
+    while True:
+        b = a+k
+        if b >= hw:
+            b = hw
+
+        c = 0
         while True:
-            b = a+k
-            if b > height:
-                b = width
-            
-            c = 0
-            while True:
-                flag_h = True
-                d = c + k
-                if d >= height:
-                    d = height
-                    flag_h = False
-                img1 = img[a : b, c: d]
-                detect(img1)
-                if flag_h == False:
-                    break
-                
-                c += 100
-            
-            a += 100
-            if a > (width-k):
+            flag_h = True
+            d = c + k
+            if d >= hw:
+                d = hw
+                flag_h = False
+            # await process_image(img, a, b, c, d)
+            await process_image(img, c, d, a, b)
+            c += 100
+            if not flag_h:
                 break
 
-def main(img):
-    img = cv2.resize(img, (1000, 1000))
-    search_Image(img)
-    BookInfo_list = []
-    for code_item in code:
-        print(code_item)
-        item = bookAPI.search_Books(code_item)
-        BookInfo_list.append(copy.deepcopy(item))
+        a += 100
+        if a > (hw-k):
+            break
 
+async def main(img):
+    BookInfo_list.clear()
     code.clear()
-    print(BookInfo_list)
-    return BookInfo_list
+    img = cv2.resize(img, (1000, 1000))
+
+    start = time.time()
+    await search_Image(img)
+    end = time.time()
+    time_diff = end - start
+
+    return BookInfo_list, time_diff
+
+
+# こいつが一番早かった
